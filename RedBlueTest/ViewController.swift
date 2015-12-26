30613dc90f18dfb3c79d20ad87e2260f15ae8fda
Todd Hassell <toddhassell@Todds-Mac-mini.local>
2015-12-26 21:26:48 +0900

    Added files for RedBlueTest

--- RedBlueTest/ViewController.swift
+++ RedBlueTest/ViewController.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var redImage: UIImageView!
+    @IBOutlet var blueImage: UIImageView!
+    @IBOutlet var redButton: UIButton!
+    @IBOutlet var blueButton: UIButton!
+    
+    @IBAction func showRed(sender: AnyObject) {
+        redImage.hidden = false
+        blueImage.hidden = true
+        redButton.hidden = true
+        blueButton.hidden = false
+    }
+    
+    @IBAction func showBlue(sender: AnyObject) {
+        blueImage.hidden = false
+        redImage.hidden = true
+        blueButton.hidden = true
+        redButton.hidden = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
